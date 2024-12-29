5e5398e2b2f9a88e21fe8db8730c32c82b699610
Bakr101 <ahmed98bakr@gmail.com>
2024-12-29 10:54:10 +0200

    started a simple server

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main(){
+	const port = "8080"
+	const filepathRoot = "."
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	server := &http.Server{
+		Addr: ":" + port,
+		Handler: mux,
+	}
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	
+	
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+}
